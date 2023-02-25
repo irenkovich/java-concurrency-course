230e2ffd3692806e00a0d6a358974a99468aa856
irenkovich <irenkovich@gmail.com>
2023-02-25 11:43:22 +0300

    MPJ(2-3/3) - Code review

--- src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
+++ src/main/java/course/concurrency/m2_async/cf/min_price/PriceAggregator.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class PriceAggregator {
     //it's impossible to set timeout for every task to 3000
     //because of test failures
-    private static final int TIMEOUT_MILLIS = 2950;
+    private static final int TIMEOUT_MILLIS = 2970;
+    private static final int HYPER_THREADING_WORKING_THREADS_NUMBER = Runtime.getRuntime().availableProcessors() * 2;
+    private final Executor executor = Executors.newFixedThreadPool(HYPER_THREADING_WORKING_THREADS_NUMBER);
 
     private PriceRetriever priceRetriever = new PriceRetriever();
 
@@ -41,8 +45,8 @@ public class PriceAggregator {
     }
 
     private CompletableFuture<Double> getPriceAsync(long shopId, long itemId) {
-        return CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(shopId, itemId))
-                .orTimeout(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)
+        return CompletableFuture.supplyAsync(() -> priceRetriever.getPrice(shopId, itemId), executor)
+                .completeOnTimeout(Double.NaN, TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)
                 .exceptionally(trw -> Double.NaN);
     }
 }
