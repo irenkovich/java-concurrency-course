f1484676d193f19f7336238184b63def484fbb9b
irenkovich <irenkovich@gmail.com>
2023-03-13 20:29:25 +0300

    MJ-371 - Fixed Notifier

--- src/main/java/course/concurrency/exams/auction/Notifier.java
+++ src/main/java/course/concurrency/exams/auction/Notifier.java
@@ -1,10 +1,13 @@
 package course.concurrency.exams.auction;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
 
 public class Notifier {
+    private static final int AVAILABLE_PROCS = Runtime.getRuntime().availableProcessors();
+    private final Executor executor = new ThreadPoolExecutor(
+            AVAILABLE_PROCS, AVAILABLE_PROCS, 0L, TimeUnit.SECONDS, new LinkedBlockingQueue<>(50000), new ThreadPoolExecutor.DiscardOldestPolicy());
     public void sendOutdatedMessage(Bid bid) {
-        CompletableFuture.runAsync(this::imitateSending);
+        CompletableFuture.runAsync(this::imitateSending, executor);
     }
 
     private void imitateSending() {
