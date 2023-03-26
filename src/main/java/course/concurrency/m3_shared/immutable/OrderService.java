a853f37c0f43f9cf79491a67dd13cfcbf224ef76
irenkovich <irenkovich@gmail.com>
2023-03-26 14:46:19 +0300

    MTJ-313 - Add reusable logic to buffer

--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -5,8 +5,9 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class OrderService {
+    private final int ONE_ITERATION_BUFF_SIZE = 1_000_000;
 
-    private final AtomicReferenceArray<Order> currentOrders = new AtomicReferenceArray<>(1_000_000);
+    private final AtomicReferenceArray<Order> ordersBuffer = new AtomicReferenceArray<>(ONE_ITERATION_BUFF_SIZE);
     private final AtomicLong nextId = new AtomicLong(0);
 
     private long nextId() {
@@ -16,32 +17,38 @@ public class OrderService {
     public long createOrder(List<Item> items) {
         Long id = nextId();
         Order order = new Order(id, items);
-        currentOrders.set(id.intValue(), order);
+        ordersBuffer.set(getIndexFromBufferById(id), order);
         return id;
     }
 
     public void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        int id = ((Long) orderId).intValue();
-        Order updated = currentOrders.updateAndGet(id, oldValue -> oldValue.setPaymentInfo(paymentInfo));
+        int id = getIndexFromBufferById(orderId);
+        Order updated = ordersBuffer.updateAndGet(id, oldValue -> oldValue.setPaymentInfo(paymentInfo));
         if (updated.checkStatus()) {
             deliver(updated);
         }
     }
 
     public void setPacked(long orderId) {
-        int id = ((Long) orderId).intValue();
-        Order updated = currentOrders.updateAndGet(id, oldValue -> oldValue.setPacked(true));
+        int id = getIndexFromBufferById(orderId);
+        Order updated = ordersBuffer.updateAndGet(id, oldValue -> oldValue.setPacked(true));
         if (updated.checkStatus()) {
             deliver(updated);
         }
     }
 
     private void deliver(Order order) {
-        currentOrders.set(order.getId().intValue(), order.setStatus(Order.Status.DELIVERED));
+        ordersBuffer.set(getIndexFromBufferById(order.getId()), order.setStatus(Order.Status.DELIVERED));
     }
 
     public boolean isDelivered(long orderId) {
-        int id = ((Long) orderId).intValue();
-        return currentOrders.get(id).getStatus().equals(Order.Status.DELIVERED);
+        int id = getIndexFromBufferById(orderId);
+        return ordersBuffer.get(id).getStatus().equals(Order.Status.DELIVERED);
     }
+
+    private int getIndexFromBufferById(Long id) {
+        long l = id - (id / ONE_ITERATION_BUFF_SIZE) * ONE_ITERATION_BUFF_SIZE;
+        return (int) l;
+    }
+
 }
